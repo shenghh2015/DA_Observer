d274d1a58ec140944f97495e37ff79baaf99369b
He <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-04-23 03:21:39 -0500

    update the cluster jobs

--- CLB_FDA/v100_cluster/job_folder/job_2.sh
+++ CLB_FDA/v100_cluster/job_folder/job_2.sh
@@ -1,2 +1,2 @@
-python train_DA1.py --gpu 2 --dis_cnn 0 --dis_fc 128 --dis_bn True --D_lr 1e-5 --G_lr 1e-5 --nD 1 --nG 1 --dAcc1 0.80 --dAcc2 0.90 --iters 100000 --bz 400 --lamda 0.001
+python train_DA1.py --gpu 2 --dis_cnn 4 --dis_fc 64 --dis_bn True --D_lr 1e-6 --G_lr 1e-6 --nD 1 --nG 1 --dAcc1 0.80 --dAcc2 0.90 --iters 100000 --bz 400 --lamda 1.0
 
--- CLB_FDA/v100_cluster/job_folder/job_3.sh
+++ CLB_FDA/v100_cluster/job_folder/job_3.sh
@@ -1,2 +1,1 @@
-python train_DA1.py --gpu 3 --dis_cnn 0 --dis_fc 128 --dis_bn True --D_lr 1e-6 --G_lr 1e-6 --nD 1 --nG 1 --dAcc1 0.80 --dAcc2 0.90 --iters 100000 --bz 400 --lamda 1.0
-
+python train_DA1.py --gpu 3 --dis_cnn 4 --dis_fc 64 --dis_bn True --D_lr 1e-6 --G_lr 1e-6 --nD 1 --nG 1 --dAcc1 0.80 --dAcc2 0.90 --iters 100000 --bz 400 --lamda 1.0
