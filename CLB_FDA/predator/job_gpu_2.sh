f56c5c64cf964a89131d303f6fd1e61b5a4e4940
shenghh <shenghh2015@gmail.com>
2020-04-25 09:00:57 -0500

    update mmd

--- CLB_FDA/predator/job_gpu_2.sh
+++ CLB_FDA/predator/job_gpu_2.sh
@@ -1,1 +1,1 @@
-python mmd_DA.py --gpu 2 --lr 1e-3 --iters 100000 --bz 400 --mmd_param 1.0 --nb_trg_labels 0
+python mmd_DA.py --gpu 2 --lr 1e-4 --iters 100000 --bz 350 --mmd_param 2.0 --nb_trg_labels 0
