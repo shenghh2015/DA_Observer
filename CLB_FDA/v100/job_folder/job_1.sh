78f3d4d80df65577582f65a25d17439e2b52c95d
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-04-27 14:10:53 -0500

    update

--- CLB_FDA/v100/job_folder/job_1.sh
+++ CLB_FDA/v100/job_folder/job_1.sh
@@ -1,2 +1,2 @@
-python mmd_DA.py --gpu 1 --docker True --shared True --source_scratch False --den_bn False --lr 1e-6 --iters 100000 --bz 400 --nb_trg_labels 300 --mmd_param 0.0 --src_clf_param 0.0 --trg_clf_param 1.0
+python mmd_DA.py --gpu 1 --docker True --shared True --source_scratch True --den_bn False --lr 1e-5 --iters 100000 --bz 400 --nb_trg_labels 300 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 1.0
 
