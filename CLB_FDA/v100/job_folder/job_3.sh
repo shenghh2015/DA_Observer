0522086525d009a2c663d5ec44e14214eb490228
He <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-04-27 02:11:39 -0500

    update v100

--- CLB_FDA/v100/job_folder/job_3.sh
+++ CLB_FDA/v100/job_folder/job_3.sh
@@ -1,1 +1,2 @@
-python mmd_DA.py --gpu 2 --docker True --shared True --lr 1e-4 --iters 100000 --bz 400 --mmd_param 2.0 --nb_trg_labels 0
+python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch True --den_bn False --lr 1e-3 --iters 100000 --bz 400 --nb_trg_labels 500 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 1.0
+
