927d2bbb5ee1b31be4cabaa69ad1bb459797cc21
He <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-04-26 20:09:36 -0500

    update mmd on v100

--- CLB_FDA/v100/job_folder/job_3.sh
+++ CLB_FDA/v100/job_folder/job_3.sh
@@ -1,1 +1,2 @@
-python train_DA1.py --gpu 3 --dis_cnn 4 --dis_fc 64 --dis_bn True --D_lr 1e-6 --G_lr 1e-6 --nD 1 --nG 1 --dAcc1 0.80 --dAcc2 0.90 --iters 100000 --bz 400 --lamda 1.0
+python mmd_DA.py --gpu 3 --docker True --shared True --lr 1e-6 --iters 100000 --bz 800 --mmd_param 1.0 --nb_trg_labels 0 --source_scratch True
+
--- CLB_FDA/v100/job_parser.py
+++ CLB_FDA/v100/job_parser.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument("job_list_file", type=str)
@@ -10,6 +11,11 @@ job_list_file = args.job_list_file
 job_folder = './job_folder'
 # job_list_file = './DA_jobs.txt'
 
+# clean the job folder
+for file in glob.glob(job_folder+'/*'):
+	os.system('rm -f {}'.format(file))
+	print('Remove {}'.format(os.path.basename(file)))
+
 job_list = []
 with open(job_list_file, 'rb') as f:
 	lines = f.readlines()
@@ -17,14 +23,11 @@ with open(job_list_file, 'rb') as f:
 		splits = line.strip().split(' ')
 		if len(splits) > 0:
 			if splits[0] == 'JOB:':
-				print(line.replace('JOB: ', ''))
+				#print(line.replace('JOB: ', ''))
 				job_list.append(line.replace('JOB: ', ''))
 
 for i in range(len(job_list)):
 	job_file = os.path.join(job_folder, 'job_{}.sh'.format(i))
-	if os.path.exists(job_file):
-		os.system('rm -f {}'.format(job_file))
 	with open(job_file, 'w+') as f:
 		f.write('{}\n'.format(job_list[i]))
-
-
+	print('Add job: {}'.format(job_list[i]))
