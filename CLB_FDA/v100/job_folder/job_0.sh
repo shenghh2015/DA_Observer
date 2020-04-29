85331929167c8a938d87ae21bfb4c939dc16d973
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-04-29 14:21:08 -0500

    v100

--- CLB_FDA/v100/job_folder/job_0.sh
+++ CLB_FDA/v100/job_folder/job_0.sh
@@ -1,2 +1,2 @@
-python mmd_DA.py --gpu 0 --docker True --shared True --source_scratch False --den_bn False --lr 1e-4 --iters 50000 --bz 400 --nb_trg_labels 100 --mmd_param 1.0 --src_clf_param 2.0 --trg_clf_param 1 --clf_v 1
+python TF.py --gpu 0 --docker True --lr 1e-6 --iters 30000 --bz 100 --nb_trg_labels 100
 
--- CLB_FDA/v100/multi_submit.sh
+++ CLB_FDA/v100/multi_submit.sh
@@ -3,4 +3,4 @@ export LSF_DOCKER_VOLUMES='/scratch1/fs1/anastasio/Data_FDA_Breast/DA_Observer:/
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_IPC=host 
 export LSF_DOCKER_SHM_SIZE=40G
-bsub -G compute-anastasio -n 4 -R 'span[ptile=4] select[mem>200000] rusage[mem=200GB]' -q anastasio-interactive -a 'docker(shenghh2020/tf_gpu_py3.5:latest)' -gpu "num=4" -o /scratch1/fs1/anastasio/Data_FDA_Breast/DA_Observer/logs/4GPU_$RANDOM /bin/bash /home/shenghuahe/DA_Observer/CLB_FDA/v100/multi_loader.sh
+bsub -G compute-anastasio -n 4 -R 'span[ptile=4] select[mem>200000] rusage[mem=200GB]' -q general-interactive -a 'docker(shenghh2020/tf_gpu_py3.5:latest)' -gpu "num=4" -o /scratch1/fs1/anastasio/Data_FDA_Breast/DA_Observer/logs/4GPU_$RANDOM /bin/bash /home/shenghuahe/DA_Observer/CLB_FDA/v100/multi_loader.sh
