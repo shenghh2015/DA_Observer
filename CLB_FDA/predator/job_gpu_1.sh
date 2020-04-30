d5e30a239e29d93f4daa58cbce86a140001b1030
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-04-30 01:54:44 -0500

    v100

--- CLB_FDA/predator/job_gpu_1.sh
+++ CLB_FDA/predator/job_gpu_1.sh
@@ -21,6 +21,6 @@
 # python TF.py --gpu 1 --docker True --lr 1e-6 --iters 20000 --bz 100 --nb_trg_labels 400
 # python mmd_DA.py --gpu 1 --docker True --shared True --source_scratch True --den_bn False --lr 1e-5 --iters 50000 --bz 400 --nb_trg_labels 100 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.5
 # python TF.py --gpu 1 --docker True --lr 1e-5 --iters 50000 --bz 100 --nb_trg_labels 400 --DA_FLAG True
-python train_source.py --gpu_num 1 --nb_cnn 6 --bn False --lr 1e-5 --nb_train 100000 --noise 2 --sig_rate 0.035 --bz 400 --optimizer Adam --nb_steps 40000 --clf_v 2
+# python train_source.py --gpu_num 1 --nb_cnn 6 --bn False --lr 1e-5 --nb_train 100000 --noise 2 --sig_rate 0.035 --bz 400 --optimizer Adam --nb_steps 40000 --clf_v 2
 
-#python TF.py --gpu 1 --docker True --source_scratch False --lr 1e-6 --iters 10000 --bz 100 --nb_trg_labels 500
+python TF.py --gpu 1 --docker True --source_scratch False --lr 1e-6 --iters 50000 --bz 100 --nb_trg_labels 500 
