b8745374636a09b825bebb94cde134b252a4a034
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-01 01:12:20 -0500

    v100

--- CLB_FDA/v100/job_folder/job_0.sh
+++ CLB_FDA/v100/job_folder/job_0.sh
@@ -1,2 +1,2 @@
-python adda_DA.py --gpu 0 --docker True --dis_cnn 0 --dis_fc 128 --dis_bn True --source_scratch True --den_bn False --clf_v 1 --lr 1e-5 --iters 200000 --bz 400 --dis_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.0 --nb_trg_labels 0
+python adda_DA.py --gpu 0 --docker True --dis_cnn 0 --dis_fc 256 --dis_bn True --source_scratch True --den_bn False --clf_v 1 --lr 1e-5 --iters 200000 --bz 400 --dis_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.0 --nb_trg_labels 0
 
