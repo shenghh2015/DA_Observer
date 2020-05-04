3df5682e7f9f9a7d90c31d1766452eb6d058714f
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-04 17:09:02 -0500

    v100

--- CLB_FDA/v100/total_val_100.py
+++ CLB_FDA/v100/total_val_100.py
@@ -61,7 +61,7 @@ def run_evaluation(model_list, model_set):
 		model_meta_files = natsorted(model_meta_files)
 		if os.path.exists(best_model_meta):
 			model_meta_files.insert(0, best_model_meta)
-		print('Amount of models:{}'.format(len(model_files)))
+		print('Amount of models:{}'.format(len(model_meta_files)))
 		best_val_auc = 0
 		select_test_auc = 0
 		val_auc_list = []
@@ -153,7 +153,7 @@ base_model = 'cnn-4-bn-False-noise-2.0-trn-100000-sig-0.035-bz-400-lr-5e-05-Adam
 os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 
 ## load target data
-_, Xt_val, Xt_tst, _, yt_val, yt_tst = load_target(dataset = 'total', train = 85000, valid = 100, test = 400)
+_, Xt_val, Xt_tst, _, yt_val, yt_tst = load_target(dataset = 'total', train = 1000, valid = 100, test = 400)
 Xt_val, Xt_tst = (Xt_val-np.min(Xt_val))/(np.max(Xt_val)-np.min(Xt_val)), (Xt_tst-np.min(Xt_tst))/(np.max(Xt_tst)-np.min(Xt_tst))
 Xt_val, Xt_tst = np.expand_dims(Xt_val, axis = 3), np.expand_dims(Xt_tst, axis = 3)
 
