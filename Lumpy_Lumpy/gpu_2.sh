86e09bc8c3cbcdcc79266ae753482ff167f5c9a6
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-14 12:25:10 -0500

    v100

---
+++ Lumpy_Lumpy/gpu_2.sh
@@ -0,0 +1,30 @@
+# python train_source.py --gpu_num 2 --nb_cnn 4 --bn False --lr 5e-5 --nb_train 100000 --noise 2.0 --sig_rate 0.035 --bz 400 --optimizer 'Adam' --nb_steps 100000
+# parser.add_argument("--gpu", type=int)
+# parser.add_argument("--docker", type = str2bool, default = True)
+# # parser.add_argument("--shared", type = str2bool, default = True)
+# parser.add_argument("--lr", type = float)
+# parser.add_argument("--iters", type = int)
+# parser.add_argument("--bz", type = int)
+# parser.add_argument("--source_scratch", type = str2bool, default = False)
+# parser.add_argument("--nb_trg_labels", type = int, default = 0)
+# parser.add_argument("--fc_layer", type = int, default = 128)
+
+# python TF.py --gpu 2 --docker True --lr 1e-5 --iters 10000 --bz 100 --source_scratch True --nb_trg_labels 400 --fc_layer 128
+# python TF.py --gpu 2 --docker True --lr 1e-5 --iters 10000 --bz 100 --source_scratch False --nb_trg_labels 500 --fc_layer 128
+# python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch True --den_bn False --lr 1e-5 --iters 50000 --bz 400 --nb_trg_labels 500 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.5
+#python train_source.py --gpu_num 2 --nb_cnn 4 --bn False --lr 1e-5 --nb_train 100000 --noise 2 --sig_rate 0.035 --bz 400 --optimizer Adam --nb_steps 50000
+# python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch True --den_bn False --lr 1e-5 --iters 50000 --bz 400 --nb_trg_labels 0 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.0 --clf_v 2
+# python train_source.py --gpu_num 2 --nb_cnn 6 --bn False --lr 1e-5 --nb_train 100000 --noise 2 --sig_rate 0.035 --bz 400 --optimizer Adam --nb_steps 100000 --clf_v 1
+
+# python TF.py --gpu 2 --docker True --lr 1e-6 --iters 50000 --bz 100 --nb_trg_labels 200 --clf_v 1 --dataset 'total'
+# python TF.py --gpu 2 --docker True --lr 1e-6 --iters 20000 --bz 100 --nb_trg_labels 400 --clf_v 1 --dataset 'dense'
+# python adda_DA.py --gpu 2 --docker True --dis_cnn 4 --dis_fc 128 --dis_bn True --source_scratch True --den_bn False --clf_v 1 --lr 1e-5 --iters 100000 --bz 400 --dis_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.05 --nb_trg_labels 70 --dataset dense
+# python adda_DA.py --gpu 2 --docker True --dis_cnn 4 --dis_fc 128 --dis_bn True --source_scratch False --den_bn False --clf_v 1 --lr 1e-5 --iters 200000 --bz 300 --dis_param 1.0 --src_clf_param 1.0 --trg_clf_param 1.0 --nb_trg_labels 100
+# python adda_DA.py --gpu 2 --docker True --dis_cnn 4 --dis_fc 128 --dis_bn True --source_scratch True --den_bn False --clf_v 1 --lr 1e-5 --iters 100000 --bz 400 --dis_param 1.0 --src_clf_param 1.0 --trg_clf_param 0.0 --nb_trg_labels 0 --dataset total
+# python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch True --den_bn False --lr 1e-5 --iters 500000 --bz 400 --nb_trg_labels 0 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 0 --clf_v 1 --dataset total
+# python total_val_100.py 2 true
+# python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch True --den_bn False --lr 5e-5 --iters 50000 --bz 400 --nb_trg_labels 0 --mmd_param 0.5 --src_clf_param 1.0 --trg_clf_param 0 --clf_v 1 --dataset total --valid 100
+# python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch False --den_bn False --lr 5e-5 --iters 20000 --bz 400 --nb_trg_labels 1000 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 1.0 --clf_v 1 --dataset total --valid 100
+# python mmd_DA.py --gpu 2 --docker True --shared True --source_scratch False --den_bn False --lr 1e-4 --iters 20000 --bz 400 --nb_trg_labels 1000 --mmd_param 1.0 --src_clf_param 1.0 --trg_clf_param 1.0 --clf_v 1 --dataset total --valid 100
+
+python adda_DA2.py --gpu 2 --docker True --dis_cnn 2 --g_cnn 6 --g_lr 1e-5 --d_lr 4e-5 --lsmooth True --dis_bn False --iters 100000 --bz 300 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 5.0 --nb_trg_labels 0 --dataset total
---
+++ Lumpy_Lumpy/load_data.py
@@ -0,0 +1,185 @@
+import os
+import glob
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
+dataset_folder = '/data/datasets'
+def load_Lumpy(docker = True, train = 100000, valid = 100, test = 400, height = 40, blur= 1.0, noise = 10):
+	if docker:
+		dataset_folder = '/data/datasets/Lumpy/h_blur/'
+	else:
+		dataset_folder = 'data/Lumpy/h_blur/'
+	if not blur == 0.5:
+		blur = int(blur)
+		sig = np.fromfile(dataset_folder+'/sig_target_b{}_blur{}.dat'.format(height, blur), dtype = np.float32).reshape(1,64,64)
+		bk = np.fromfile(dataset_folder+'/b_target_b{}_blur{}.dat'.format(height, blur), dtype = np.float32).reshape(100400,64,64)
+	else:
+		sig = np.fromfile(dataset_folder+'/sig_source_b{}_blur{}.dat'.format(height, blur), dtype = np.float32).reshape(1,64,64)
+		bk = np.fromfile(dataset_folder+'/b_source_b{}_blur{}.dat'.format(height, blur), dtype = np.float32).reshape(100400,64,64)
+	print('Lumpy data shape: sig {}, bk {}'.format(sig.shape, bk.shape))
+	if blur == 0.5:
+	elif blur == 1.0:
+		seed = 0
+	elif blur == 2.0:
+		seed = 1
+	elif blur == 3.0:
+		seed = 2
+	elif blur == 4.0:
+		seed = 3
+	elif blur == 5.0:
+		seed = 4
+	noise = np.random.RandomState(seed).normal(0, nosie, bk.shape)
+	sig_absent = bk + noise
+	sig_present = sig + bk + noise
+	X_trn = np.concatenate([sig_absent[:train,:], sig_present[:train,:]])
+	X_val = np.concatenate([sig_absent[100000:100000+valid,:], sig_present[100000:100000+valid,:]])
+	X_tst = np.concatenate([sig_absent[100200:100200+test,:], sig_present[100200:100200+test,:]])
+	y_trn = np.concatenate([np.zeros((train, 1)), np.ones((train, 1))]).flatten()
+	y_val = np.concatenate([np.zeros((valid, 1)), np.ones((valid, 1))]).flatten()
+	y_tst = np.concatenate([np.zeros((test, 1)), np.ones((test, 1))]).flatten()
+	return X_trn, X_val, X_tst, y_trn, y_val, y_tst
+
+def load_source(train = 80000, valid = 400, test = 400, sig_rate = 0.035):
+# 	train = 80000
+# 	valid = 400
+# 	test = 400
+# 	sig_rate = 0.035
+# 	sig_file = '/shared/planck/Phantom/Breast_Xray/FDA_signals/hetero_sig.dat'
+# 	CLB_file = '/shared/rsaas/shenghua/CLB/CLB_128N_400000IM.npy'
+	sig_file = os.path.join(dataset_folder, 'FDA_signals/hetero_sig.dat')
+	CLB_file = os.path.join(dataset_folder, 'CLB/CLB_128N_400000IM.npy')
+	sig = np.fromfile(sig_file, dtype = np.float32).reshape(109,109)
+	data = np.load(CLB_file, mmap_mode='r')
+	X = data[:,:,0:train+valid+test]
+	X = np.transpose(np.reshape(X, [128, 128, train+valid+test], order='F'))
+	X_SA_trn, X_SA_val, X_SA_tst = X[:train, 64-54:64+55,64-54:64+55], X[train:train+valid, 64-54:64+55,64-54:64+55],\
+		X[train+valid:train+valid+test, 64-54:64+55,64-54:64+55]
+	X_SP_trn, X_SP_val, X_SP_tst = X_SA_trn + sig * sig_rate, X_SA_val + sig * sig_rate, X_SA_tst + sig * sig_rate
+	X_trn, X_val, X_tst = np.concatenate([X_SA_trn, X_SP_trn]), np.concatenate([X_SA_val, X_SP_val]), np.concatenate([X_SA_tst, X_SP_tst])
+	y_trn = np.concatenate([np.zeros((train,1)), np.ones((train,1))]).flatten()
+	y_val = np.concatenate([np.zeros((valid,1)), np.ones((valid,1))]).flatten()
+	y_tst = np.concatenate([np.zeros((test,1)), np.ones((test,1))]).flatten()
+
+	return X_trn, X_val, X_tst, y_trn, y_val, y_tst
+
+def load_target(dataset = 'total', train = 80000, valid = 400, test = 400):
+# 	dataset = 'total'
+# 	train = 80000
+# 	valid = 400
+# 	test = 400
+# 	X_SA = np.load('/shared/planck/Phantom/Breast_Xray/FDA_DM_ROIs/npy_dataset/{}_SA.npy'.format(dataset))
+# 	X_SP = np.load('/shared/planck/Phantom/Breast_Xray/FDA_DM_ROIs/npy_dataset/{}_SP.npy'.format(dataset))
+	if dataset == 'dense':
+		offset_valid = 7100
+	elif dataset == 'hetero':
+		offset_valid = 36000
+	elif dataset == 'scattered':
+		offset_valid = 33000
+	elif dataset == 'fatty':
+		offset_valid = 9000
+	elif dataset == 'total':
+		offset_valid = 85000
+	offset_test = 400 + offset_valid
+	X_SA = np.load(os.path.join(dataset_folder, 'FDA_DM_ROIs/npy_dataset/{}_SA.npy'.format(dataset)))
+	X_SP = np.load(os.path.join(dataset_folder, 'FDA_DM_ROIs/npy_dataset/{}_SP.npy'.format(dataset)))
+	X_SA_trn, X_SA_val, X_SA_tst = X_SA[:train,:], X_SA[offset_valid:offset_valid+valid,:], X_SA[offset_test:offset_test+test,:]
+	X_SP_trn, X_SP_val, X_SP_tst = X_SP[:train,:], X_SP[offset_valid:offset_valid+valid,:], X_SP[offset_test:offset_test+test,:]
+	X_trn, X_val, X_tst = np.concatenate([X_SA_trn, X_SP_trn]), np.concatenate([X_SA_val, X_SP_val]), np.concatenate([X_SA_tst, X_SP_tst])
+	y_trn = np.concatenate([np.zeros((train,1)), np.ones((train,1))]).flatten()
+	y_val = np.concatenate([np.zeros((valid,1)), np.ones((valid,1))]).flatten()
+	y_tst = np.concatenate([np.zeros((test,1)), np.ones((test,1))]).flatten()
+	print('---- Dataset Summary: {} ----'.format(dataset))
+	print(' -all SA {}, SP {}'.format(X_SA.shape[0], X_SP.shape[0]))
+	print(' -trn SA {}, SP {}'.format(X_SA_trn.shape[0], X_SP_trn.shape[0]))
+	print(' -val SA {}, SP {}'.format(X_SA_val.shape[0], X_SP_val.shape[0]))
+	print(' -val SA {}, SP {}'.format(X_SA_tst.shape[0], X_SP_tst.shape[0]))
+# 	print('\n')
+	return X_trn, X_val, X_tst, y_trn, y_val, y_tst
+
+def load_target_archive(dataset = 'total', train = 80000, valid = 400, test = 400):
+# 	dataset = 'total'
+# 	train = 80000
+# 	valid = 400
+# 	test = 400
+	X_SA = np.fromfile(os.path.join(dataset_folder, 'FDA_DM_ROIs/npy_dataset/{}_SA.dat'.format(dataset)), dtype = np.float32)
+	X_SA = X_SA.reshape(-1,109,109)
+	shuff = np.random.RandomState(2).permutation(X_SA.shape[0])
+	X_SA = X_SA[shuff]
+	X_SP = np.fromfile(os.path.join(dataset_folder, 'FDA_DM_ROIs/npy_dataset/{}_SA.dat'.format(dataset)), dtype = np.float32)
+	X_SP = X_SP.reshape(-1,109,109)
+	shuff = np.random.RandomState(3).permutation(X_SP.shape[0])
+	X_SP = X_SP[shuff]
+	X_SA_trn, X_SA_val, X_SA_tst = X_SA[:train,:], X_SA[train:train+valid,:], X_SA[train+valid:train+valid+test,:]
+	X_SP_trn, X_SP_val, X_SP_tst = X_SP[:train,:], X_SP[train:train+valid,:], X_SP[train+valid:train+valid+test,:]
+	X_trn, X_val, X_tst = np.concatenate([X_SA_trn, X_SP_trn]), np.concatenate([X_SA_val, X_SP_val]), np.concatenate([X_SA_tst, X_SP_tst])
+	y_trn = np.concatenate([np.zeros((train,1)), np.ones((train,1))]).flatten()
+	y_val = np.concatenate([np.zeros((valid,1)), np.ones((valid,1))]).flatten()
+	y_tst = np.concatenate([np.zeros((test,1)), np.ones((test,1))]).flatten()
+	print('---- Dataset Summary: {} ----'.format(dataset))
+	print(' -all SA {}, SP {}'.format(X_SA.shape[0], X_SP.shape[0]))
+	print(' -trn SA {}, SP {}'.format(X_SA_trn.shape[0], X_SP_trn.shape[0]))
+	print(' -val SA {}, SP {}'.format(X_SA_val.shape[0], X_SP_val.shape[0]))
+	print(' -val SA {}, SP {}'.format(X_SA_tst.shape[0], X_SP_tst.shape[0]))
+# 	print('\n')
+	return X_trn, X_val, X_tst, y_trn, y_val, y_tst
+
+def evaluate_target_HO(dataset = 'total', train = 80000, valid = 400, test = 400):
+# 	dataset = 'dense'
+# 	train = 7000
+# 	valid = 400
+# 	test = 400
+	X_trn, X_val, X_tst, y_trn, y_val, y_tst = load_target(dataset = dataset, train = train, valid = valid, test = test)
+	H0, H1 = X_trn[:train,:], X_trn[train:,:]
+	H0, H1 = H0.reshape(H0.shape[0],-1), H1.reshape(H1.shape[0],-1)
+	K0, K1 = np.cov(H0.T), np.cov(H1.T)
+	K, dg = 0.5*(K0+K1), (np.mean(H1, axis = 0)-np.mean(H0, axis = 0))
+	W = np.matmul(np.linalg.inv(K),dg)
+	H_tst = X_tst.reshape(X_tst.shape[0],-1)
+	scores = np.matmul(H_tst, W)
+	auc_score = roc_auc_score(y_tst, scores.flatten())
+	print('HO AUC:{0:.3f}'.format(auc_score))
+#                 trn_h0 = bk[:100000,:,:]
+#                 shp = trn_h0.shape
+#                 trn_h0 = np.reshape(trn_h0, (shp[0],-1))
+#                 Kb = np.cov(trn_h0.T)
+#                 sigma = 20
+#                 Kn = sigma**2*np.eye(64*64)
+#                 K = Kb + Kn
+#                 K_inv = np.linalg.inv(K)
+#                 sig_ = sig.flatten()
+#                 W = np.matmul(K_inv,sig_)
+# 
+#                 # plt.ion()
+#                 # plt.imshow(W.reshape((64,64))
+# 
+#                 tst_h0 = bk[100000:100000+100,:,:].reshape((100,64*64))
+#                 tst_h0 = np.random.RandomState(12).normal(tst_h0, 20)
+#                 tst_h1 = tst_h0 + sig_
+#                 tst_h1 = np.random.RandomState(12).normal(tst_h1, 20)
+#                 test_data = np.concatenate([tst_h0,tst_h1], axis = 0)
+#                 y_scores = np.matmul(test_data, W)
+#                 y_true = np.concatenate([np.zeros(100), np.ones(100)])
+#                 auc_score = roc_auc_score(y_true, y_scores)
+
+# import matplotlib.pyplot as plt
+# plt.close('all')   
+# plt.ion()
+# fig = plt.figure()
+# for i in range(len(X_trn)):
+# 	if i > 100:
+# 		break
+# 	ax = fig.add_subplot(231)
+# 	bx = fig.add_subplot(232)
+# 	cx = fig.add_subplot(233)
+# 	ax1 = fig.add_subplot(234)
+# 	bx1 = fig.add_subplot(235)
+# 	cx1 = fig.add_subplot(236)
+# 	ax.imshow(X_SA_trn[i,:])
+# 	bx.imshow(X_SA_val[i,:])
+# 	cx.imshow(X_SA_tst[i,:])
+# 	ax1.imshow(X_SP_trn[i,:])
+# 	bx1.imshow(X_SP_val[i,:])
+# 	cx1.imshow(X_SP_tst[i,:])
+# 	plt.pause(1)
+
+# def load_target():
