fb41d82e8ae2f4341cb5efe27bb665c9f6e43120
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-15 18:23:50 -0500

    v100

---
+++ Lumpy_Lumpy/models2.py
@@ -0,0 +1,152 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+def bn_layer(x, scope, is_training, epsilon=0.001, decay=0.99, reuse=None):
+    """
+    Performs a batch normalization layer
+
+    Args:
+        x: input tensor
+        scope: scope name
+        is_training: python boolean value
+        epsilon: the variance epsilon - a small float number to avoid dividing by 0
+        decay: the moving average decay
+
+    Returns:
+        The ops of a batch normalization layer
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        shape = x.get_shape().as_list()
+        # gamma: a trainable scale factor
+        gamma = tf.get_variable("gamma", shape[-1], initializer=tf.constant_initializer(1.0), trainable=True)
+        # beta: a trainable shift value
+        beta = tf.get_variable("beta", shape[-1], initializer=tf.constant_initializer(0.0), trainable=True)
+        moving_avg = tf.get_variable("moving_avg", shape[-1], initializer=tf.constant_initializer(0.0), trainable=False)
+        moving_var = tf.get_variable("moving_var", shape[-1], initializer=tf.constant_initializer(1.0), trainable=False)
+        if is_training:
+            # tf.nn.moments == Calculate the mean and the variance of the tensor x
+            avg, var = tf.nn.moments(x, np.arange(len(shape)-1), keep_dims=True)
+            avg=tf.reshape(avg, [avg.shape.as_list()[-1]])
+            var=tf.reshape(var, [var.shape.as_list()[-1]])
+            #update_moving_avg = moving_averages.assign_moving_average(moving_avg, avg, decay)
+            update_moving_avg=tf.assign(moving_avg, moving_avg*decay+avg*(1-decay))
+            #update_moving_var = moving_averages.assign_moving_average(moving_var, var, decay)
+            update_moving_var=tf.assign(moving_var, moving_var*decay+var*(1-decay))
+            control_inputs = [update_moving_avg, update_moving_var]
+        else:
+            avg = moving_avg
+            var = moving_var
+            control_inputs = []
+        with tf.control_dependencies(control_inputs):
+            output = tf.nn.batch_normalization(x, avg, var, offset=beta, scale=gamma, variance_epsilon=epsilon)
+
+    return output
+
+
+def bn_layer_top(x, scope, is_training, epsilon=0.001, decay=0.99):
+    """
+    Returns a batch normalization layer that automatically switch between train and test phases based on the 
+    tensor is_training
+
+    Args:
+        x: input tensor
+        scope: scope name
+        is_training: boolean tensor or variable
+        epsilon: epsilon parameter - see batch_norm_layer
+        decay: epsilon parameter - see batch_norm_layer
+
+    Returns:
+        The correct batch normalization layer based on the value of is_training
+    """
+    #assert isinstance(is_training, (ops.Tensor, variables.Variable)) and is_training.dtype == tf.bool
+
+    return tf.cond(
+        is_training,
+        lambda: bn_layer(x=x, scope=scope, epsilon=epsilon, decay=decay, is_training=True, reuse=None),
+        lambda: bn_layer(x=x, scope=scope, epsilon=epsilon, decay=decay, is_training=False, reuse=True),
+    )
+
+def weight_variable(shape, name):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial, name=name)
+
+def bias_variable(shape, name):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial, name=name)
+
+def conv2d(x, w):
+    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding="SAME")
+
+def lrelu(x, alpha=0.2):
+  return tf.nn.relu(x) - alpha * tf.nn.relu(-x)
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding="SAME")
+
+l2_regularizer = tf.contrib.layers.l2_regularizer(1e-5)
+def _conv_bn_lrelu_pool(x, pool = False, bn = True, bn_training = True, scope = 'conv'):
+	_conv = tf.layers.conv2d(x, filters = 32, kernel_size = [5,5], strides=(1, 1), padding='same',
+			kernel_initializer= 'truncated_normal', kernel_regularizer=l2_regularizer)
+	if bn:
+# 		_bn = tf.layers.batch_normalization(_conv, training = True)
+		_bn = bn_layer_top(_conv, scope = scope, is_training = bn_training, epsilon=0.001, decay=0.99)
+	else:
+		_bn = _conv
+	_lrelu = tf.nn.leaky_relu(_bn)
+	if pool:
+		_out = max_pool_2x2(_lrelu)
+	else:
+		_out = _lrelu
+	return _out
+
+def conv_block(x, nb_cnn = 4, bn = False, scope_name = 'base', bn_training = True):
+	with tf.variable_scope(scope_name):
+		h = _conv_bn_lrelu_pool(x, pool = False, bn = bn, bn_training = bn_training, scope = 'conv0')
+		for i in range(1, nb_cnn):
+			if i%2 == 1:
+				pool = True
+			else:
+				pool = False
+			h = _conv_bn_lrelu_pool(h, pool = pool, bn = bn, bn_training = bn_training, scope = 'conv{}'.format(i))
+	return h
+
+# x = tf.placeholder("float", shape=[None, 109,109, 1])
+# h = conv_block(x, nb_cnn = 4, bn = True, scope_name = 'base')
+
+def dense_block(x, fc_layers = [128, 1], bn = False, scope_name = 'base', bn_training = True):
+# 	shape = x.shape.as_list()[1:]
+	with tf.variable_scope(scope_name):
+		flat = tf.layers.flatten(x)
+		h1 = tf.layers.dense(flat, fc_layers[0], kernel_regularizer=l2_regularizer)
+		if bn:
+# 			h1 = tf.layers.batch_normalization(h1, training = True)
+			h1 = bn_layer_top(_conv, scope = 'dense', is_training = bn_training, epsilon=0.001, decay=0.99)
+		h1 = tf.nn.leaky_relu(h1)
+		h2 = tf.layers.dense(h1, fc_layers[1], kernel_regularizer = l2_regularizer)
+	return h1, h2
+
+### create network
+def conv_classifier(x, nb_cnn = 4, fc_layers = [128,1],  bn = False, scope_name = 'base', reuse = False, bn_training = True):
+	with tf.variable_scope(scope_name, reuse = reuse):
+		conv_net = conv_block(x, nb_cnn = nb_cnn, bn = bn, scope_name = 'conv', bn_training = bn_training)
+		h, pred_logit = dense_block(conv_net, fc_layers = fc_layers, bn = bn, scope_name = 'classifier', bn_training = bn_training)
+
+	return conv_net, h, pred_logit
+
+# def conv_classifier2(x, nb_cnn = 4, fc_layers = [128,1],  bn = False, scope_name = 'base', reuse = False):
+# 	with tf.variable_scope(scope_name, reuse = reuse):
+# 		conv_net = conv_block(x, nb_cnn = nb_cnn, bn = bn, scope_name = 'conv')
+# 		h, pred_logit = dense_block(conv_net, fc_layers = fc_layers, bn = True, scope_name = 'classifier')
+# 
+# 	return conv_net, h, pred_logit
+
+def discriminator(x, nb_cnn = 2, fc_layers = [128, 1], bn = True, reuse = False, bn_training = True):
+	with tf.variable_scope('discriminator', reuse = reuse):
+		if nb_cnn > 0:
+			h = conv_block(x, nb_cnn = nb_cnn, bn = bn, scope_name = 'cov', bn_training = bn_training)
+			_, pred_logit = dense_block(h, fc_layers = fc_layers, bn = bn, scope_name = 'fc', bn_training = bn_training)
+		else:
+			_, pred_logit = dense_block(x, fc_layers = fc_layers, bn = bn, scope_name = 'fc', bn_training = bn_training)
+	return pred_logit
---
+++ Lumpy_Lumpy/train_source2.py
@@ -0,0 +1,271 @@
+import tensorflow as tf
+import tensorflow.math as tm
+
+import numpy as np
+import os
+import glob
+from natsort import natsorted
+from termcolor import colored 
+import argparse
+from sklearn.metrics import roc_auc_score
+import scipy.io
+import time
+from functools import partial
+
+# user-defined tools
+from load_data import load_Lumpy 
+# from model import conv_classifier
+from models2 import conv_classifier
+
+def str2bool(value):
+    return value.lower() == 'true'
+
+def plot_LOSS(file_name, train_loss_list, val_loss_list, test_loss_list):
+	import matplotlib.pyplot as plt
+	from matplotlib.backends.backend_agg import FigureCanvasAgg
+	from matplotlib.figure import Figure
+	fig_size = (8,6)
+	fig = Figure(figsize=fig_size)
+	ax = fig.add_subplot(111)
+	ax.plot(train_loss_list)
+	ax.plot(val_loss_list)
+	ax.plot(test_loss_list)
+	title = os.path.basename(os.path.dirname(file_name))
+	ax.set_title(title)
+	ax.set_xlabel('Iterations')
+	ax.set_ylabel('Loss')
+	ax.legend(['Train','Val','Test'])
+	ax.set_xlim([0,len(train_loss_list)])
+	canvas = FigureCanvasAgg(fig)
+	canvas.print_figure(file_name, dpi=100)
+
+def plot_AUC(file_name, train_list, val_list, test_list):
+	import matplotlib.pyplot as plt
+	from matplotlib.backends.backend_agg import FigureCanvasAgg
+	from matplotlib.figure import Figure
+	fig_size = (8,6)
+	fig = Figure(figsize=fig_size)
+	file_name = file_name
+	ax = fig.add_subplot(111)
+	ax.plot(train_list)
+	ax.plot(val_list)
+	ax.plot(test_list)
+	title = os.path.basename(os.path.dirname(file_name))
+	ax.set_title(title)
+	ax.set_xlabel('Iterations')
+	ax.set_ylabel('AUC')
+	ax.legend(['Train','Valid','Test'])
+	ax.set_xlim([0,len(train_list)])
+	canvas = FigureCanvasAgg(fig)
+	canvas.print_figure(file_name, dpi=100)
+
+# generate the folder
+def generate_folder(folder):
+    import os
+    if not os.path.exists(folder):
+        os.system('mkdir -p {}'.format(folder))
+
+def compute_pairwise_distances(x, y):
+    if not len(x.get_shape()) == len(y.get_shape()) == 2:
+        raise ValueError('Both inputs should be matrices.')
+    if x.get_shape().as_list()[1] != y.get_shape().as_list()[1]:
+        raise ValueError('The number of features should be the same.')
+
+    norm = lambda x: tf.reduce_sum(tf.square(x), 1)
+    return tf.transpose(norm(tf.expand_dims(x, 2) - tf.transpose(y)))
+
+def gaussian_kernel_matrix(x, y, sigmas):
+    beta = 1. / (2. * (tf.expand_dims(sigmas, 1)))
+    dist = compute_pairwise_distances(x, y)
+    s = tf.matmul(beta, tf.reshape(dist, (1, -1)))
+    return tf.reshape(tf.reduce_sum(tf.exp(-s), 0), tf.shape(dist))
+
+
+def maximum_mean_discrepancy(x, y, kernel=gaussian_kernel_matrix):
+    cost = tf.reduce_mean(kernel(x, x))
+    cost += tf.reduce_mean(kernel(y, y))
+    cost -= 2 * tf.reduce_mean(kernel(x, y))
+    cost = tf.where(cost > 0, cost, 0, name='value')
+    return cost
+
+def bias_variable(shape, name):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial, name=name)
+
+# plot and save the file
+def plot_loss(file_name, loss, val_loss):
+	f_out = file_name
+	from matplotlib.backends.backend_agg import FigureCanvasAgg
+	from matplotlib.figure import Figure
+	start_idx = 0
+	if len(loss)>start_idx:
+		title = os.path.basename(os.path.dirname(file_name))
+		fig = Figure(figsize=(8,6))
+		ax = fig.add_subplot(1,1,1)
+		ax.plot(loss[start_idx:],'b-',linewidth=1.3)
+		ax.plot(val_loss[start_idx:],'r-',linewidth=1.3)
+		ax.set_title(title)
+		ax.set_ylabel('Loss')
+		ax.set_xlabel('Iterations/100')
+		ax.legend(['source-loss', 'mmd-loss'], loc='upper left')  
+		canvas = FigureCanvasAgg(fig)
+		canvas.print_figure(f_out, dpi=80)
+
+def plot_auc(target_file_name, val_auc_list, target_auc_list):
+	import matplotlib.pyplot as plt
+	from matplotlib.backends.backend_agg import FigureCanvasAgg
+	from matplotlib.figure import Figure
+	fig_size = (8,6)
+	fig = Figure(figsize=fig_size)
+	file_name = target_file_name
+	ax = fig.add_subplot(111)
+	ax.plot(target_auc_list)
+	ax.plot(val_auc_list)
+	title = os.path.basename(os.path.dirname(file_name))
+	ax.set_title(title)
+	ax.set_xlabel('Iterations')
+	ax.set_ylabel('AUC')
+	ax.legend(['Test','Val'])
+	ax.set_xlim([0,len(target_auc_list)])
+	canvas = FigureCanvasAgg(fig)
+	canvas.print_figure(file_name, dpi=100)
+
+def plot_hist(file_name, x, y):
+	import matplotlib.pyplot as plt
+	from matplotlib.backends.backend_agg import FigureCanvasAgg
+	from matplotlib.figure import Figure
+	kwargs = dict(alpha=0.6, bins=100, density= False, stacked=True)
+	fig_size = (8,6)
+	fig = Figure(figsize=fig_size)
+	file_name = file_name
+	ax = fig.add_subplot(111)
+	ax.hist(x, **kwargs, color='g', label='SA')
+	ax.hist(y, **kwargs, color='r', label='SP')
+	title = os.path.basename(os.path.dirname(file_name))
+	ax.set_title(title)
+	ax.set_xlabel('statistics')
+	ax.set_ylabel('Frequency')
+	ax.legend(['SA', 'SP'])
+	ax.set_xlim([np.min(np.concatenate([x,y])), np.max(np.concatenate([x,y]))])
+	canvas = FigureCanvasAgg(fig)
+	canvas.print_figure(file_name, dpi=100)
+
+def print_yellow(str):
+	from termcolor import colored 
+	print(colored(str, 'yellow'))
+
+def print_red(str):
+	from termcolor import colored 
+	print(colored(str, 'red'))
+
+def print_green(str):
+	from termcolor import colored 
+	print(colored(str, 'green'))
+
+def print_block(symbol = '*', nb_sybl = 70):
+	print_red(symbol*nb_sybl)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu", type=int, default = 2)
+parser.add_argument("--docker", type = str2bool, default = True)
+parser.add_argument("--model_type", type = str, default = 'source')
+parser.add_argument("--lr", type = float, default = 1e-5)
+parser.add_argument("--bz", type = int, default = 300)
+parser.add_argument("--train", type = int, default = 100000)
+parser.add_argument("--nb_cnn", type = int, default = 4)
+parser.add_argument("--fc_layer", type = int, default = 128)
+parser.add_argument("--bn", type = str2bool, default = False)
+parser.add_argument("--h", type = float, default = 40)
+parser.add_argument("--blur", type = float, default = 0.5)
+parser.add_argument("--noise", type = float, default = 10)
+parser.add_argument("--valid", type = int, default = 100)
+parser.add_argument("--test", type = int, default = 200)
+parser.add_argument("--iters", type = int, default = 10000)
+
+args = parser.parse_args()
+print(args)
+
+gpu = args.gpu
+docker = args.docker
+model_type = args.model_type
+batch_size = args.bz
+nb_steps = args.iters
+lr = args.lr
+nb_cnn = args.nb_cnn
+fc_layer = args.fc_layer
+bn = args.bn
+h =args.h
+blur = args.blur
+noise = args.noise
+train = args.train
+valid = args.valid
+test = args.test
+
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+if docker:
+	output_folder ='/data/results/'
+else:
+	output_folder = 'data/results/'
+print(output_folder)
+data_folder = os.path.join(output_folder,'Lumpy/{}'.format(model_type))
+# load source data
+Xs_trn, Xs_val, Xs_tst, ys_trn, ys_val, ys_tst = load_Lumpy(docker = docker, train = train, valid = valid, test = test, height = h, blur= blur, noise = noise)
+Xs_trn, Xs_val, Xs_tst = np.expand_dims(Xs_trn, axis = 3), np.expand_dims(Xs_val, axis = 3), np.expand_dims(Xs_tst, axis = 3)
+ys_trn, ys_val, ys_tst = ys_trn.reshape(-1,1), ys_val.reshape(-1,1), ys_tst.reshape(-1,1)
+
+model_name = 'Lumpy-{}-cnn-{}-fc-{}-lr-{}-bz-{}-bn-{}-T_V_T-{}k_{}_{}-{}_{}_{}-itrs-{}-v2'\
+				.format(model_type,nb_cnn,fc_layer, lr,batch_size,bn, int(train/1000), valid, test, h, blur,noise, nb_steps)
+model_folder = os.path.join(data_folder, model_name); generate_folder(model_folder)
+
+img_size = 64
+xs = tf.placeholder("float", shape=[None, img_size, img_size, 1])
+ys = tf.placeholder("float", shape=[None, 1])
+is_training = tf.placeholder_with_default(False, (), 'is_training')
+
+conv_net_src, h_src, source_logit = conv_classifier(xs, nb_cnn = nb_cnn, fc_layers = [fc_layer,1],  bn = bn, scope_name = model_type, bn_training = is_training)
+
+source_vars_list = tf.trainable_variables(model_type)
+source_key_list = [v.name[:-2].replace(model_type, 'base') for v in tf.trainable_variables(model_type)]
+source_key_direct = {}
+for key, var in zip(source_key_list, source_vars_list):
+	source_key_direct[key] = var
+source_saver = tf.train.Saver(source_key_direct, max_to_keep=nb_steps)
+
+# source loss
+src_clf_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = ys, logits = source_logit))
+source_trn_ops = tf.train.AdamOptimizer(lr).minimize(src_clf_loss, var_list = source_vars_list)
+
+train_loss_list, val_loss_list, test_loss_list = [], [], []
+train_auc_list, val_auc_list, test_auc_list = [], [], []
+
+best_val_auc = -np.inf
+with tf.Session() as sess:
+	tf.global_variables_initializer().run(session=sess)
+	for iteration in range(nb_steps):
+		indices = np.random.randint(0, Xs_trn.shape[0]-1, batch_size)
+		# train the source
+		source_x = Xs_trn[indices,:]; source_y = ys_trn[indices,:]; sess.run(source_trn_ops, feed_dict={xs:source_x, ys: source_y, is_training: True})
+		if iteration%100 == 0:
+			train_loss = src_clf_loss.eval(session=sess, feed_dict={xs:source_x, ys:source_y, is_training: False})
+			val_loss = src_clf_loss.eval(session=sess, feed_dict={xs:Xs_val, ys:ys_val, is_training: False})
+			test_loss = src_clf_loss.eval(session=sess, feed_dict={xs:Xs_tst, ys:ys_tst, is_training: False})
+			train_stat = source_logit.eval(session=sess, feed_dict={xs:source_x, is_training: False}); train_auc = roc_auc_score(source_y, train_stat)
+			val_stat = source_logit.eval(session=sess, feed_dict={xs:Xs_val, is_training: False}); val_auc = roc_auc_score(ys_val, val_stat)
+			test_stat = source_logit.eval(session=sess, feed_dict={xs:Xs_tst, is_training: False}); test_auc = roc_auc_score(ys_tst, test_stat)
+			train_loss_list, train_auc_list = np.append(train_loss_list, train_loss), np.append(train_auc_list, train_auc)
+			val_loss_list, val_auc_list = np.append(val_loss_list, val_loss), np.append(val_auc_list, val_auc)
+			test_loss_list, test_auc_list = np.append(test_loss_list, test_loss), np.append(test_auc_list, test_auc)
+			np.savetxt(model_folder+'/train_loss.txt', train_loss_list);np.savetxt(model_folder+'/train_auc.txt', train_auc_list)
+			np.savetxt(model_folder+'/val_loss.txt', val_loss_list);np.savetxt(model_folder+'/val_auc.txt', val_auc_list)
+			np.savetxt(model_folder+'/test_loss.txt', test_loss_list);np.savetxt(model_folder+'/test_auc.txt', test_auc_list)
+			plot_LOSS(model_folder+'/loss-{}.png'.format(model_name), train_loss_list, val_loss_list, test_loss_list)
+			plot_AUC(model_folder+'/auc-{}.png'.format(model_name), train_auc_list, val_auc_list, test_auc_list)
+			print_block(symbol = '-', nb_sybl = 70)
+			print_green('Loss: train {0:.4f} val {1:.4f} test {2:.4f}; AUC: train {3:.4f} val {4:.4f} test {5:.4f}; iter {6:}'.format(train_loss, val_loss, test_loss, train_auc, val_auc, test_auc, iteration))
+			print(model_name)
+			if best_val_auc < val_auc:
+				best_val_auc = val_auc
+				np.savetxt(model_folder+'/best_stat.txt', test_stat)
+				source_saver.save(sess, model_folder +'/best')
+				plot_hist(model_folder +'/hist-{}.png'.format(model_name), test_stat[:int(len(test_stat)/2)], test_stat[int(len(test_stat)/2):])
