d60559ca9ccbeccf1c3f5c27cc3497f7528caed9
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-18 10:38:45 -0500

    v100

--- CLB_FDA/predator/gpu_0.sh
+++ CLB_FDA/predator/gpu_0.sh
@@ -68,5 +68,5 @@
 
 # python adda_DA3.py --gpu 0 --docker True --dis_cnn 2 --g_cnn 6 --g_lr 1e-5 --d_lr 4e-5 --lsmooth True --dis_bn False --iters 100000 --bz 300 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 3.0 --nb_trg_labels 0 --dataset total
 
-python adda_DA3.py --gpu 0 --docker True --dis_cnn 2 --g_cnn 6 --g_lr 1e-5 --d_lr 4e-5 --lsmooth False --dis_bn True --den_bn True --iters 100000 --bz 300 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 3.0 --nb_trg_labels 0 --dataset total
+python adda_DA3.py --gpu 0 --docker True --dis_cnn 2 --g_cnn 4 --g_lr 1e-5 --d_lr 4e-5 --lsmooth False --dis_bn True --den_bn True --iters 100000 --bz 300 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
 
