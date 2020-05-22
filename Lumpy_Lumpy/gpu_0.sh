e80184eb7f574fea7467058841db1d6cad380e2b
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-05-22 00:03:50 -0500

    v100

--- Lumpy_Lumpy/gpu_0.sh
+++ Lumpy_Lumpy/gpu_0.sh
@@ -1,2 +1,4 @@
 # python adda_DA.py --gpu 0 --docker True --dis_cnn 0 --dis_fc 256 --shared False --scratch False --dis_bn True
-python mmd_DA.py --gpu 0 --shared True --lr 1e-4 --iters 10000 --bz 300 --scratch False --bn False
+# python mmd_DA.py --gpu 0 --shared True --lr 1e-4 --iters 10000 --bz 300 --scratch False --bn False
+
+python adda_DA.py --gpu 0 --shared True --lr 1e-5 --d_lr 4e-5 --g_lr 1e-5 --iters 10000 --bz 300 --scratch True --nb_source 100000 --nb_target 100000 --dis_bn True --t_blur 3.0
