7b77bf51095fbdaf1764a297658761346145581b
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-06-18 01:15:25 -0500

    v100

--- FDA/p0.sh
+++ FDA/p0.sh
@@ -1,2 +1,2 @@
 ## June 18
-python mmd_adda.py --gpu 0 --docker True --dis_cnn 5 --g_cnn 5 --g_lr 1e-5 --d_lr 1e-5 --dis_bn True --den_bn True --iters 400000 --bz 128 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
+python mmd_adda.py --gpu 0 --docker True --dis_cnn 5 --g_cnn 5 --dis_fc 256 --g_lr 1e-5 --d_lr 1e-5 --dis_bn True --den_bn True --iters 400000 --bz 128 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
