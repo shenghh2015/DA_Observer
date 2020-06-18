495049be7681cd3b02ec79a23f11b0388c94c5d8
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-06-18 17:20:58 -0500

    v100

--- FDA/p2.sh
+++ FDA/p2.sh
@@ -1,2 +1,2 @@
 ## June 18
-python mmd_wd.py --gpu 2 --docker True --dis_cnn 4 --g_cnn 4 --g_lr 1e-5 --d_lr 1e-5 --lr 1e-6 --dis_bn True --den_bn True --iters 400000 --bz 200 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 0.1 --nb_trg_labels 0 --dataset dense
+python mmd_wd.py --gpu 2 --docker True --dis_cnn 4 --g_cnn 4 --g_lr 1e-5 --d_lr 1e-5 --lr 1e-5 --dis_bn True --den_bn True --iters 400000 --bz 150 --dis_param 1.0 --trg_clf_param 0 --src_clf_param 0.1 --nb_trg_labels 0 --dataset total
