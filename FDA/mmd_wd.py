49c9e386ee8b56bdb85aa3782afa6d80dd3c1aa0
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-06-19 01:14:21 -0500

    v100

--- FDA/mmd_wd.py
+++ FDA/mmd_wd.py
@@ -284,8 +284,8 @@ mixed_input = lerp(reals, fakes, mixing_factors)
 mixed_scores = discriminator(mixed_input, nb_cnn = dis_cnn, fc_layers = [dis_fc, 1], bn = dis_bn, reuse = True, drop = drop, bn_training = dis_training)
 mixed_loss = tf.reduce_sum(mixed_scores)
 mixed_grads = tf.gradients(mixed_loss, [mixed_input])[0]
-mixed_norms = tf.reduce_mean(tf.sqrt(tf.reduce_sum(tf.square(mixed_grads), axis=[1,2,3])))
-gradient_penalty = tf.square(mixed_norms - wgan_target)
+mixed_norms = tf.sqrt(tf.reduce_sum(tf.square(mixed_grads), axis=[1,2,3]))
+gradient_penalty = tf.reduce_mean(tf.square(mixed_norms - wgan_target))
 disc_loss += gradient_penalty * (wgan_lambda / (wgan_target**2))
 
 epsilon_penalty = tf.reduce_mean(tf.square(real_scores_out))
