4ca14ece4cba14854977d9b8c8a16161c62ce45f
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-06-18 22:50:45 -0500

    v100

--- FDA/job_folder/job_3.sh
+++ FDA/job_folder/job_3.sh
@@ -1,2 +1,2 @@
-python mmd_wd.py --gpu 3 --docker True --dis_cnn 4 --dis_fc 256 --g_cnn 4 --g_lr 5e-6 --d_lr 5e-6 --lr 5e-6 --dis_bn True --den_bn True --iters 400000 --bz 200 --dis_param 1.0 --mmd_param 1.0 --trg_clf_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
+python mmd_wd.py --gpu 3 --docker True --dis_cnn 4 --dis_fc 256 --g_cnn 4 --g_lr 1e-5 --d_lr 1e-5 --lr 1e-5 --dis_bn False --den_bn True --iters 400000 --bz 200 --dis_param 1.0 --mmd_param 0 --trg_clf_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
 
