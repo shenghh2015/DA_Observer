ad70ef033dde0f12b2cae8a1c2df74ad153a6fd6
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-06-19 01:24:37 -0500

    v100

--- FDA/p0.sh
+++ FDA/p0.sh
@@ -1,2 +1,2 @@
 ## June 18
-python mmd_wd_check.py --gpu 0 --docker True --dis_cnn 4 --g_cnn 4 --dis_fc 128 --g_lr 1e-5 --d_lr 1e-5 --lr 1e-5 --dis_bn False --den_bn True --iters 400000 --bz 100 --dis_param 1.0 --trg_clf_param 0 --mmd_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
+python mmd_wd_check2.py --gpu 0 --docker True --dis_cnn 4 --g_cnn 4 --dis_fc 128 --g_lr 1e-5 --d_lr 1e-5 --lr 1e-5 --dis_bn False --den_bn True --iters 400000 --bz 100 --dis_param 1.0 --trg_clf_param 0 --mmd_param 0 --src_clf_param 1.0 --nb_trg_labels 0 --dataset total
